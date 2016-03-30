8669a3d1b026b2c2009a50262f3a7d69a4a0db1e
Sameh Kamal <sameh.kamaleldin@gmail.com>
2016-03-30 16:26:47 +0100

    Update bootstrap.sh

---
+++ provision/bootstrap.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# ==============================================================================
+# = author        :- sameh kamal
+# = description   :- provisioning a machine with spark
+# = preconditions :- runs on ubuntu 14.4.x
+# ==============================================================================
